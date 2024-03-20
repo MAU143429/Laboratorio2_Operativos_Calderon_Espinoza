ccbab9d8d62fb270969c3bdcafb57fb541a27edb
MAU143429 <mau141226@estudiantec.cr>
2024-03-20 16:56:52 -0600

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,21 +1,28 @@
 import time
 
 def factorial(n):
-
     # Factorial de 0
     if n == 0:
         return 1
 
     # Factorial de n-1.
     else:
-        print ("Calculando el factorial de: " + str(n))
+        print("Calculando el factorial de:", n)
         return n * factorial(n-1)
 
+def cpu_intensive_task():
+    # Realiza una tarea intensiva en CPU
+    for _ in range(600000000):  # Aumenta el número de iteraciones para prolongar la ejecución
+        pass
 
 numero = 900
-inicio = time.time()  
+inicio = time.time()
+
+# Realiza la tarea intensiva en CPU
+cpu_intensive_task()
+
 resultado = factorial(numero)
-fin = time.time() 
+fin = time.time()
+
 print(f"El factorial de {numero} es {resultado}")
 print(f"Tiempo de ejecución: {fin - inicio} segundos")
-time.sleep(1)
